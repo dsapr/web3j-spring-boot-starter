aef498bafe68ad99c636a652e75523671e7cbf29
chenyi wang <785722840@qq.com>
2023-02-07 16:00:50 +0800

    first commit

---
+++ src/test/java/com/meta/web3jspring/Web3jSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta.web3jspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web3jSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
